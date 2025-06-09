a0a88b64015ccb465f9860c13c74caf8f37bce07
hmmrfll <a.kazakevych@gmail.com>
2025-06-09 12:12:45 +0300

    decomposition

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',  // Прокси для запросов к серверу на Node.js
+export default defineConfig(({ mode, command }) => {
+  const env = process.env;
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.FRONTEND_PORT || '5173'),
+      host: '0.0.0.0',
+      allowedHosts: ['shoes-gmc-camcorders-trial.trycloudflare.com'],
+      proxy: {
+        '/api': 'http://localhost:5000',
+      },
     },
-  },
+    preview: {
+      port: parseInt(env.FRONTEND_PORT || '5173'),
+      host: '0.0.0.0'
+    },
+  };
 });
